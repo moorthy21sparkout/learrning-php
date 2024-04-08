9dca933c75ef642ddb8eaf88efa1c331c8df8e1a
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-08 15:17:35 +0530

    Update static method.php

---
+++ static method.php
@@ -0,0 +1,16 @@
+<?php
+class car
+  {
+   static  function display()
+    {
+      return "AUDI";
+    }
+    function __construct()
+
+    {
+      echo "the car name is : .self :: display()"    }
+    
+  }
+$obj=new car();
+
+?>
