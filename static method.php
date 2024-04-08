48975d61cb6c104a0adad2f6ab7f91d18b711cf9
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-08 15:30:43 +0530

    Update static method.php

--- static method.php
+++ static method.php
@@ -15,3 +15,22 @@ class car
 $obj=new car();
 
 ?>
+<?php
+class car
+  {
+   static  function display()
+    {
+      return "AUDI";
+    }
+    class BMW extends car
+    {
+      function __construct()
+        {
+          echo "the super car name is:".parent::display();
+        }
+  
+    }
+$obj=new BMW();
+$obj->car();
+
+?>
